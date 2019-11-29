618ff3f08d8ce0a75902959a62f2d0515c1b71a6
NAYEONSEUNG <58206041+NAYEONSEUNG@users.noreply.github.com>
2019-11-29 16:43:17 +0900

    생성자, 로그인 암호
    ㅇㅇㅇ

---
+++ chapter05/ysys.java
@@ -0,0 +1,22 @@
+package chapter05;
+
+import java.util.Scanner;
+
+public class ysys {
+
+	public void main(String []args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("입력>>");
+		int sum = sc.nextInt();
+		int num1 = 7;
+		int num2 = 5;
+		int num3 = 6;
+		
+		
+		
+	}
+	
+	
+}
